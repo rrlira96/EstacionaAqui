1604522e5f3227403324dcd712c1bbccfde49f04
rrlira96 <renato.lira.14@gmail.com>
2022-06-10 10:42:49 -0300

    Fixing app class

--- src/main/java/br/unicap/engsoftware/EstacionaAqui/EstacionaAquiApplication.java
+++ src/main/java/br/unicap/engsoftware/EstacionaAqui/EstacionaAquiApplication.java
@@ -3,15 +3,10 @@ package br.unicap.engsoftware.EstacionaAqui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-<<<<<<< HEAD
-=======
-
->>>>>>> 2d54e52aec65e4f0df64af4df71bd9e4d55b0288
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-<<<<<<< HEAD
 public class EstacionaAquiApplication {
 
     public static void main(String[] args) {
@@ -27,24 +22,5 @@ public class EstacionaAquiApplication {
             }
         };
     }
-=======
-
-public class EstacionaAquiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EstacionaAquiApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer webConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
-			}
-		};
-	}
->>>>>>> 2d54e52aec65e4f0df64af4df71bd9e4d55b0288
-
 
 }
